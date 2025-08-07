d0ece3fb4e0aa6ef90db0152d90a7db19d046bce
santa <xixixixxddjddj@gmail.com>
2025-08-07 13:34:35 +0300

    правки в offer

--- scripts/blocks/burger_manu.js
+++ scripts/blocks/burger_manu.js
@@ -1,13 +1,8 @@
-/*
-  тут, як і з js для слайдера,
-  зроби функцію-обгортку і все туди перекинь,
-  потім експортуй і імпортуй в main.js
-*/
-
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.header__nav');
-
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-  nav.classList.toggle('active');
-});
+export function initBurgerMenu() {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.header__nav');
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+  });
+}
--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,10 @@ import { initSlider } from './blocks/slider.js';
 document.addEventListener('DOMContentLoaded', () => {
   initSlider();
 });
+
+
+import { initBurgerMenu } from './blocks/burger_manu.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initBurgerMenu();
+});
