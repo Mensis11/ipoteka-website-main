ba92eb7613cc128eaf2350a6a5a7ddd203ae8915
santa <xixixixxddjddj@gmail.com>
2025-08-07 16:08:46 +0300

    header about us

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,9 @@
-import { initSlider } from './blocks/slider.js';
+import { initSlider } from './blocks/slider.js'; 
+import { initBurgerMenu } from './blocks/burger_manu.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initSlider();
 });
-
-
-import { initBurgerMenu } from './blocks/burger_manu.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initBurgerMenu();
